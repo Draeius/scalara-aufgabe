dbadac42b4f5ab4581472107e314260d0a4fda2e
Draeius <matthias@woelk-web.de>
2024-12-19 21:14:21 +0100

    removed unnecessary import

--- src/modules/db/db.module.ts
+++ src/modules/db/db.module.ts
@@ -1,12 +1,10 @@
 import {Module} from "@nestjs/common";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {dataSourceOptions} from "./data-source";
-import {Person} from "../../entities/person.entity";
 
 @Module({
     imports: [
         TypeOrmModule.forRoot(dataSourceOptions),
-        TypeOrmModule.forFeature([Person])
     ],
     exports: [TypeOrmModule],
 })
