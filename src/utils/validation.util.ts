053c3abe2c54da22f551ff9a39c14c776c910cf9
Draeius <matthias@woelk-web.de>
2024-12-19 20:16:05 +0100

    fixed naming and doc

--- src/utils/validation.util.ts
+++ src/utils/validation.util.ts
@@ -2,17 +2,17 @@ import {HttpException} from "@nestjs/common";
 
 export class ValidationUtil {
     /**
-     * Parses and
+     * Parses and validates the provided ID.
      *
-     * @param {string} personId The persons Id as a string.
+     * @param {string} id The ID as a string.
      * @returns {number} The parsed number.
      * @throws {HttpException} If the provided ID is not a valid number > 0.
      * @private
      */
-    public static parseID(personId: string): number {
-        const parsedId = parseInt(personId);
+    public static parseID(id: string): number {
+        const parsedId = parseInt(id);
         if (isNaN(parsedId) || parsedId <= 0) {
-            throw new HttpException(`No valid ID provided. ID: ${personId}`, 422);
+            throw new HttpException(`No valid ID provided. ID: ${id}`, 422);
         }
         return parsedId;
     }
